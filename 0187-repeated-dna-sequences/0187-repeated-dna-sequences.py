94c3f12712bfcde20bf23bc7f72bb912470d81d9
Arshia Yousefnia <arshia.yousefnia@gmail.com>
2025-07-16 16:27:18 +0330

    Time: 28 ms (34.63%), Space: 28.7 MB (88.81%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        table = defaultdict(int)
+
+        for i in range(len(s) - 9):
+            string = s[i:i + 10]
+
+            table[string] += 1
+
+        out = []
+        for i, val in table.items():
+            if val > 1:
+                out.append(i)
+        
+        return out
+        
